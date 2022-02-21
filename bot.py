d7f6be289e207fecc0f6e29a87b5fb525c579f09
configtheworld <erkinture@gmail.com>
2022-02-22 02:58:21 +0300

    trader added

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-from operator import pos
 import os
 from binance import Client
 import pandas as pd
@@ -21,3 +20,47 @@ def change_position(current, buy=True):
     else:
         position_frame.loc[position_frame.Currency == current, "position"] = 0
     position_frame.to_csv("position", index=False)
+
+
+def gethourlydata(symbol):
+    frame = pd.DataFrame(client.get_historical_klines(
+        symbol, "1h", "25 hours ago UTC"))
+    frame = frame.iloc[:, :5]
+    frame.columns = ["Time", "Open", "High", "Low", "Close"]
+    frame[["Open", "High", "Low", "Close"]] = frame[[
+        "Open", "High", "Low", "Close"]].astype(float)
+    frame.Time = pd.to_datetime(frame.Time, unit="ms")
+    return frame
+
+
+df = gethourlydata("BTCUSDT")
+
+
+def applytechnicals(df):
+    df["FastSMA"] = df.Close.rolling(7).mean()
+    df["SlowSMA"] = df.Close.rolling(25).mean()
+
+
+def trader(curr):
+    # how much buy or sell
+    qty = position_frame[position_frame.Currency == curr].quantity.values[0]
+    # get data
+    df = gethourlydata(curr)
+    applytechnicals(df)
+    lastrow = df.iloc[-1]
+
+    if not position_frame[position_frame.Currency == curr].quantity.values[0]:
+        if lastrow.FastSMA > lastrow.SlowSMA:
+            # order = client.create_order(symbol=curr,side="BUY",type="MARKET",quantity=qty)
+            # print (order)
+            change_position(curr, buy=True)
+        else:
+            print(f"Not in position{curr} but Condition not fulfilled")
+    else:
+        print(f"Already in {curr} position")
+        if lastrow.SlowSMA > lastrow.FastSMA:
+            # order = client.create_order(symbol=curr,side="SELL",type="MARKET",quantity=qty)
+            # print (order)
+            change_position(curr, buy=False)
+        else:
+            print(f"Not in position{curr} but Condition not fulfilled")
